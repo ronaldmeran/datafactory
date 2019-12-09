35e308cd6b621f11340f06a98a769300c963f49f
Ronald Meran <ronald.meran@ipricegroup.com>
2019-12-09 23:04:08 +0800

    Add monitoring

---
+++ src/datafactory/monitoring.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def print_activity_run_details(activity_run):
+    """Print activity run details."""
+    print("\n\tActivity run details\n")
+    print("\tActivity run status: {}".format(activity_run.status))
+    if activity_run.status == 'Succeeded':
+        print("\tNumber of bytes read: {}".format(
+            activity_run.output['dataRead']))
+        print("\tNumber of bytes written: {}".format(
+            activity_run.output['dataWritten']))
+        print("\tCopy duration: {}".format(
+            activity_run.output['copyDuration']))
+    else:
+        print("\tErrors: {}".format(activity_run.error['message']))
