bc1d1ef493ecd4e2b63c4ef030d99b6e29cc62c1
Ronald Meran <ronaldmeran@users.noreply.github.com>
2020-10-22 21:44:49 +0800

    Update monitoring.py

--- src/datafactory/monitoring.py
+++ src/datafactory/monitoring.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+import logging
+
+
 def print_activity_run_details(activity_run):
     """Print activity run details."""
-    print("\n\tActivity run details\n")
-    print("\tActivity run status: {}".format(activity_run.status))
+    logging.info("Activity run details")
+    logging.info("Activity run status: {}".format(activity_run.status))
     if activity_run.status == 'Succeeded':
-        print("\tNumber of bytes read: {}".format(
+        logging.info("Number of bytes read: {}".format(
             activity_run.output['dataRead']))
-        print("\tNumber of bytes written: {}".format(
+        logging.info("Number of bytes written: {}".format(
             activity_run.output['dataWritten']))
-        print("\tCopy duration: {}".format(
+        logging.info("Copy duration: {}".format(
             activity_run.output['copyDuration']))
     else:
-        print("\tErrors: {}".format(activity_run.error['message']))
+        logging.error("Errors: {}".format(activity_run.error['message']))
